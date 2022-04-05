e5a4fc338fc7607fe6d4185ea1097a6a4f0981f3
yamamoto.ryota <yamamoto.ryota@HL00832.local>
2022-04-05 11:00:21 +0900

    Initial commit

---
+++ test/lambda-aurora.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaAurora from '../lib/lambda-aurora-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-aurora-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaAurora.LambdaAuroraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
